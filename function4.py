20eb621b4a862ffad45d6d9ccd772f0f4cf8a159
udaygarg6789 <106262885+udaygarg6789@users.noreply.github.com>
2022-05-30 17:07:46 +0530

    Write a function to tell user if he/she is able to vote or not.

---
+++ function4.py
@@ -0,0 +1,7 @@
+age = int(input("Enter age of a user: "))
+
+def eligible(age):
+    if age >= 18:
+         print("User is eligible for voting: ", age)
+    else:
+         print("User is not eligible for voting: ", age)
